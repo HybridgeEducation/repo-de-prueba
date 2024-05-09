b75d51933ee1cb748733555b72e54a8fd986e77e
Gerardo Mathus <gerardo@nextia.mx>
2024-05-08 20:10:16 -0400

    primer commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# repo-de-prueba
+Repositorio de prueba para mostrar todo el power de git, github y git cli
+
+
+## Estamos haciendo cambios al README
+
+$$
+f(x) = x^2
+$$
+
+no funciona LaTex
---
+++ main.py
@@ -0,0 +1,8 @@
+#Autor: Gerardo Mathus
+#fecha: 23 de agosto de 2019
+
+print("Hola mundo")
+print("Soy un archivo de python 3")
+suma = 5 + 15
+print("la suma es", suma)
+#Un comentario por aquí
